ce9e796dba67f019ef4b1186bfb1d79afbdc88a5
Gurpreet Singh <progurpreetsingh@gmail.com>
2022-02-06 21:04:12 +0530

    Add try-catch for HTTP Errors

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const todoRouter = require("./routes/todos");
 
 app.use(express.json());
 app.use("/api/todos", todoRouter);
+app.use((req, res) => {
+  res.status(404).json({ message: "Not Found" });
+});
 
 const port = process.env.PORT || 3000;
-app.listen(3000, console.log(`Server listening on port ${port}`));
+app.listen(3000, console.log(`Server listening on port ${port}...`));
--- controllers/todos.js
+++ controllers/todos.js
@@ -1,18 +1,33 @@
 const Todo = require("../models/todos");
 
 const getAllTodos = async (req, res) => {
-  const todos = await Todo.find({});
-  res.json(todos);
+  try {
+    const todos = await Todo.find({});
+    res.json(todos);
+  } catch (error) {
+    res.status(500).json();
+  }
 };
 
 const getTodo = async (req, res) => {
-  const todo = await Todo.findById(req.params.id);
-  res.json(todo);
+  try {
+    const todo = await Todo.findById(req.params.id);
+    if (!todo) {
+      return res.status(404).json({ message: "Todo Not Found" });
+    }
+    res.json(todo);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
 };
 
 const createTodo = async (req, res) => {
-  const todo = await Todo.create(req.body);
-  res.status(201).json(todo);
+  try {
+    const todo = await Todo.create(req.body);
+    res.status(201).json(todo);
+  } catch (error) {
+    res.status(400).json({ message: "Invalid Request" });
+  }
 };
 
 module.exports = { getAllTodos, getTodo, createTodo };
